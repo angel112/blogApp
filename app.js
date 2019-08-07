1be9e98f9c39a5d7a133649693bef5cbaf16dcf0
angel112 <ngllakra@gmail.com>
2019-08-08 02:25:53 +0530

    created server listener

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express'),
+mongoose = require('mongoose'),
+bodyParser = require('body-parser'),
+app = express();
+
+
+
+
+
+app.listen(3000, function(){
+    console.log("Server has started");
+});
